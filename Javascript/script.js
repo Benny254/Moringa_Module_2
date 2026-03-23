7ae82482a8bd541f1ed561d1553d5ae4a9d34c14
Benny <asshimbenny01@gmail.com>
2026-03-23 11:30:54 +0300

    updated intro to js

---
+++ Javascript/script.js
@@ -0,0 +1,48 @@
+let name = "Ben"
+    //name = "Benjamin"
+    //name = "Titus"
+
+console.log("Hello " + name);
+
+var age = 30;
+console.log("You are " + age + " years old");
+
+var height = 1.8;
+console.log("You are " + height + " meters tall");
+
+var weight = 80;
+console.log("You are " + weight + " kilograms heavy");
+
+var isMarried = false;
+console.log("Are you married? " + isMarried);
+
+var isAlive = true;
+console.log("Are you alive? " + isAlive);
+
+var isCool = true;
+console.log("Are you cool? " + isCool);
+
+var isTall = false;
+console.log("Are you tall? " + isTall);
+
+var isFat = false;
+console.log("Are you fat? " + isFat);
+
+var isGood = true;
+console.log("Are you good? " + isGood);
+
+var isBad = false;
+console.log("Are you bad? " + isBad);
+
+var isHealthy = true;
+console.log("Are you healthy? " + isHealthy);
+
+let fruits = ["apple", "banana", "orange"];
+console.log("You like " + fruits[0] + ", " + fruits[1] + " and " + fruits[2]);
+
+let vegetables= ["carrot", "spinach", "broccoli"];
+console.log("You like " + vegetables[0] + ", " + vegetables[1] + " and " + vegetables[2]);
+
+let students = ["john", "jane", "joe"];
+console.log("You like " + students[0] + ", " + students[1] + " and " + students[2] + " in class");
+
